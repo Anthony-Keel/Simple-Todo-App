3a55ded04f2a579d31b543a838082dc6d47f3b5a
anthony keel <farmtireguy@gmail.com>
2020-08-13 11:53:03 -0500

    initial commit

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,69 @@
+import React from "react"
+import TodosList from "./TodosList.js"
+import Header from "./Header.js"
+import InputTodo from "./InputTodo.js"
+import {v4 as uuidv4} from "uuid"
+
+class TodoContainer extends React.Component{
+   state = {
+        todos: [
+        {
+            id: uuidv4(),
+            title: "Setup development enviroment",
+            completed: true
+        },
+        {
+            id: uuidv4(),
+            title: "Develop website and add content",
+            completed: false
+        },
+        {
+            id: uuidv4(),
+            title: "Deploy to live server",
+            completed: false
+        }
+        ]
+        };
+        handleChange = id =>{
+            this.setState({
+                todos: this.state.todos.map(todo => {
+                    if (todo.id === id){
+                        todo.completed = !todo.completed;
+                    }
+                    return todo;
+                })
+            })
+        };
+        delTodo = id => {
+            this.setState({
+                    todos: [
+                        ...this.state.todos.filter(todo => { 
+                         return todo.id !== id;    
+                          })   
+                         ]
+            });
+                };
+    addTodoItem = title =>{
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false
+        };
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
+    };
+    render(){
+        return (
+            <div className="conatiner">
+                <Header />
+                <InputTodo addTodoProps={this.addTodoItem} />
+               <TodosList 
+               todos={this.state.todos} 
+               handleChangeProps={this.handleChange} 
+               deleteTodoProps = {this.delTodo} />
+            </div>
+        );
+    }
+}
+export default TodoContainer
