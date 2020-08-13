5d909e7e72348bddba1d83ccdf87f25a72d68b32
anthony keel <farmtireguy@gmail.com>
2020-08-13 12:08:25 -0500

    Updates

---
+++ precache-manifest.598b7ce73b61ff33bdbb0fb630bd8bfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319ca79351d3ebd39661ec66009a1146",
+    "url": "/Simple-Todo-App/index.html"
+  },
+  {
+    "revision": "2117e143bee51b3e600d",
+    "url": "/Simple-Todo-App/static/css/main.9795f0bf.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/Simple-Todo-App/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Todo-App/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2117e143bee51b3e600d",
+    "url": "/Simple-Todo-App/static/js/main.6634b8a7.chunk.js"
+  },
+  {
+    "revision": "b7cbabd1aa620b4972d4",
+    "url": "/Simple-Todo-App/static/js/runtime-main.189dfa3e.js"
+  }
+]);
